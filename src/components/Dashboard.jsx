ad1de2a30c085dd6e4fc1f50c15b56f1e2842a38
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-09-19 21:54:08 +0530

    feat: logout button added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,15 @@
+import { supabase } from "./supabase"
 
-export default function Dashboard () {
+export default function Dashboard() {
 
-    return(
-        <div>
+    async function logouthandler() {
+        await supabase.auth.signOut()
+    }
+
+    return (
+        <div className="flex justify-center items-center flex-col h-screen w-screen">
             <p className="text-center text-4xl text-cyan-700">Welocome To Dashboard</p>
+            <button onClick={logouthandler} className="mt-8 border-1 border-blue-700">LOGOUT</button>
         </div>
     )
 }
